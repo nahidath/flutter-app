5a3a794447610f6425ca0164f6164fc902131223
nahidath <36660735+nahidath@users.noreply.github.com>
2024-05-01 17:58:23 -0400

    learning and starting new app

---
+++ myapp/lib/pages/home/widgets/category.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:gamestore/pages/home/widgets/newest.dart';
+import 'package:gamestore/pages/home/widgets/popular.dart';
+
+class CategorySection extends StatelessWidget {
+  CategorySection({super.key});
+
+  final categories = [
+    {
+      'icon': Icons.face_retouching_natural,
+      'color': const Color(0xFF605CF4),
+      'title': 'Visage'
+    },
+    {
+      'icon': Icons.accessibility_new,
+      'color': const Color(0xFFFC77A6),
+      'title': 'Corps'
+    },
+    {
+      'icon': Icons.face_3,
+      'color': const Color(0xFF4391FF),
+      'title': 'Cheveux'
+    },
+    {
+      'icon': Icons.add_circle,
+      'color': const Color(0xFF7182f2),
+      'title': 'Plus'
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFFF6F8FF),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        children: [
+          Container(
+            height: 140,
+            padding: const EdgeInsets.all(25),
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) => Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: categories[index]['color'] as Color),
+                    child: Icon(
+                      categories[index]['icon'] as IconData,
+                      color: Colors.white,
+                      size: 40,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    categories[index]['title'] as String,
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.7),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+              separatorBuilder: (_, index) => const SizedBox(width: 33),
+              itemCount: categories.length,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            width: 410,
+            child: const Text(
+              'Articles populaires',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          PopularGame(),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            width: 410,
+            child: const Text(
+              'Nouveaux articles',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          NewestGame(),
+        ],
+      ),
+    );
+  }
+}
