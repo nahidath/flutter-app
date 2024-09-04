f2fdbc8cbc0513392ff9df570c758adf821f7b20
nahidath <36660735+nahidath@users.noreply.github.com>
2024-09-04 18:43:38 -0400

    favorite page started

---
+++ myapp/lib/pages/favorite/favorite.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class Favorite extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favorite'),
+      ),
+      body: Center(
+        child: Text('Favorite'),
+      ),
+    );
+  }
+}
