5914923defb0681c557ec529237ec0a4f49bca39
nahidath <36660735+nahidath@users.noreply.github.com>
2024-09-12 16:44:28 -0400

    working on modifyI nfo page

--- myapp/lib/pages/profile/subpages/modifyInfo.dart
+++ myapp/lib/pages/profile/subpages/modifyInfo.dart
@@ -15,46 +15,77 @@ class _ModifyInfoState extends State<ModifyInfo> {
    // return Container();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Modifier les informations'),
+        title: Text('Modifier les informations', style: TextStyle(color: const Color(0xFF41210b), fontWeight: FontWeight.bold, fontSize: 20)),
+        elevation: 1,
+        scrolledUnderElevation: 1,
+        surfaceTintColor: const Color(0xFFD7CCC8),
+        shadowColor: Theme.of(context).colorScheme.shadow,
         backgroundColor: const Color(0xFFF3E7E4),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: const BoxDecoration(
-          color: Color(0xFFF3E7E4),
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: const BoxDecoration(
+            color: Color(0xFFF3E7E4),
           ),
-        ),
-        child: Column(
-          children: [
-            ProfilePicture(
-              imagePath: 'assets/img/user-no-photo.png',
-              radius: 60,
-              isEdit: true,
-              key: Key('profile-picture'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Nom',
+          child: Column(
+            children: [
+              ProfilePicture(
+                imagePath: 'assets/img/user-no-photo.png',
+                radius: 60,
+                isEdit: true,
+                key: Key('profile-picture'),
               ),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Prénom',
+              const SizedBox(height: 40),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Nom',
+                  labelStyle: TextStyle(
+                    color: Color(0xFF41210b),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.edit, color: Color(0xFF41210b)),
+                    onPressed: () => {
+                      print('edit'),
+                    },),
+                ),
+                style: const TextStyle(
+                  color: Color(0xFF41210b),
+                ),
               ),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email',
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Prénom',
+                  labelStyle: TextStyle(
+                    color: Color(0xFF41210b),
+                  ),
+                  suffixIcon: Icon(Icons.edit, color: Color(0xFF41210b)),
+                ),
+                style: const TextStyle(
+                  color: Color(0xFF41210b),
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Enregistrer'),
-            ),
-          ],
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                    color: Color(0xFF41210b),
+                  ),
+                  suffixIcon: Icon(Icons.edit, color: Color(0xFF41210b)),
+                ),
+                style: const TextStyle(
+                  color: Color(0xFF41210b),
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Enregistrer', style: const TextStyle(
+                  color: Color(0xFF41210b),
+                ),),
+              ),
+            ],
+          ),
         ),
       ),
     );
